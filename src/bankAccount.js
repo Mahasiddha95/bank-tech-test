c1ee79fd0264b900b195c4badf18dd546b8d33d9
Valerio Franchi <vlrfranchi1@gmail.com>
2023-01-24 16:34:15 +0000

    Other tests on Withdrawal passed

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -16,5 +16,21 @@ class BankAccount {
         balance: this.balance
       });
     }
+
+    withdrawal(amount) {
+      if(typeof amount !== "number" || amount <= 0) {
+        throw new Error("Invalid withdrawal amount");
+      }
+      if(amount > this.balance) {
+        throw new Error("Insufficient balance");
+      }
+      this.balance -= amount;
+      this.history.push({
+        date: new Date(),
+        type: 'withdrawal',
+        amount: amount,
+        balance: this.balance
+      });
+    };
   }
   module.exports = BankAccount;
